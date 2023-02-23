c27f27dc9ec06d67b95ebc69087cfc96e4b0bd10
Hein Thu <74117599+H4CK3RG0D@users.noreply.github.com>
2023-02-23 13:15:31 -0600

    Create admin.js

---
+++ admin/admin.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Admin Page</title>
+  </head>
+  <body>
+    <h1>Admin Page</h1>
+    <form method="post" action="check_login.php">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required><br><br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required><br><br>
+      <input type="submit" value="Login">
+    </form>
+  </body>
+</html>
