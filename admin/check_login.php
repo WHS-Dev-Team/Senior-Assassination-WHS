ff355c6b538ebc5c9e3ffc1e9ebff66037de4507
Hein Thu <74117599+H4CK3RG0D@users.noreply.github.com>
2023-02-23 13:14:59 -0600

    Update check_login.php

--- admin/check_login.php
+++ admin/check_login.php
@@ -8,7 +8,7 @@
     exit();
   } else {
     // Redirect to the permission denied page
-    header('Location: permission_denied.php');
+    header('Location: admin/permission_denied.php');
     exit();
   }
 ?>
