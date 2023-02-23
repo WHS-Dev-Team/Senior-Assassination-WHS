3b0b187b4f22aa231cb86d5be9d79c93f09ea6b2
Hein Thu <74117599+H4CK3RG0D@users.noreply.github.com>
2023-02-23 13:07:18 -0600

    Create check_login.php

---
+++ scripts/check_login.php
@@ -0,0 +1,14 @@
+<?php
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  
+  if ($username === 'admin' && $password === 'password') {
+    // Redirect to the admin page
+    header('Location: admin.php');
+    exit();
+  } else {
+    // Redirect to the permission denied page
+    header('Location: permission_denied.php');
+    exit();
+  }
+?>
