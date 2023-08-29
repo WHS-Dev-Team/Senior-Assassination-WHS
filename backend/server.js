83a743f2fae4c1278b3b859bc275f3c63a59e553
H4CK3RG0D <74117599+H4CK3RG0D@users.noreply.github.com>
2023-08-29 16:50:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { connectToMongoDB } = require('./mongoconnect'); // Import the connectToMongoDB function
-const apiRoutes = require('./routes/api'); // Import the apiRoutes
-const handlerRoutes = require('./handler/handler'); // Import the handlerRoutes
+const connectToMongoDB = require('./mongoconnect');
+const apiRoutes = require('./routes/api');
 
 const app = express();
-
-// Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, '../frontend')))
-// Middleware to parse JSON bodies
-app.use(bodyParser.json())
-// Mount the apiRoutes under the '/api' prefix
+app.use(bodyParser.json());
 app.use('/api', apiRoutes);
-// Mount the handlerRoutes under the '/handlers' prefix
-app.use('/handlers', handlerRoutes);
 
-// Connect to MongoDB and start the server
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, '../fronten')));
+
+// Connect to MongoDB
 connectToMongoDB()
   .then(() => {
+    // All other routes will be handled by the frontend app
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../fronten', 'index.html'));
+    });
+
+    // Start the server
     const port = process.env.PORT || 6969;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
